cd61d66429359b9debb71a4a1c38e16842ed47fa
ankit-srivas <ascontinentalgt@gmail.com>
2024-04-29 23:00:14 -0400

    Adding Polymorphism Assignment

---
+++ Polymorphism Assignment/Polymorphism Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// Define an interface called IQuittable
+public interface IQuittable
+{
+    // Define a void method called Quit()
+    void Quit();
+}
+
+// Define an Employee class that inherits from the IQuittable interface
+public class Employee : IQuittable
+{
+    // Implement the Quit() method defined in the interface
+    public void Quit()
+    {
+        Console.WriteLine("Employee quits the job.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an object of type IQuittable using polymorphism
+        IQuittable employee = new Employee();
+
+        // Call the Quit() method on the object
+        employee.Quit();
+    }
+}
